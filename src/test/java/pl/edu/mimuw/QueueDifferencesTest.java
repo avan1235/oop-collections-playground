a19742b8c70ca10a2a67ec25c28790a9de5a6bfc
Aleshkev <43296251+Aleshkev@users.noreply.github.com>
2022-06-01 15:31:02 +0200

    sum testz

--- src/test/java/pl/edu/mimuw/QueueDifferencesTest.java
+++ src/test/java/pl/edu/mimuw/QueueDifferencesTest.java
@@ -1,5 +1,80 @@
 package pl.edu.mimuw;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.*;
+
 class QueueDifferencesTest {
 
+  @Nested
+  class DifferencesInIterationOrder {
+    @Test
+    void iterateArrayDequeue() {
+      var q = new ArrayDeque<>(List.of(1, 3, 2, 4));
+      var z = new ArrayList<>(q);
+      assertEquals(List.of(1, 3, 2, 4), z);
+    }
+
+    @Test
+    void iterateLinkedList() {
+      Queue<Integer> q = new LinkedList<>(List.of(1, 3, 2, 4));
+      var z = new ArrayList<>(q);
+      assertEquals(List.of(1, 3, 2, 4), z);
+    }
+
+    @Test
+    void iteratePriorityQueue() {
+      var q = new PriorityQueue<>(List.of(1, 3, -1, 2, 4));
+      var z = new ArrayList<>(q);
+      z.sort(Integer::compare); // No order guaranteed.
+      assertEquals(List.of(-1, 1, 2, 3, 4), z);
+    }
+  }
+
+  @Nested
+  class DifferencesInPeek {
+    @Test
+    void peekArrayDequeue() {
+      var q = new ArrayDeque<>(List.of(1, 3, 2, 4));
+      assertEquals(1, q.peek());
+    }
+
+    @Test
+    void peekLinkedList() {
+      Queue<Integer> q = new LinkedList<>(List.of(1, 3, 2, 4));
+      assertEquals(1, q.peek());
+    }
+
+    @Test
+    void peekPriorityQueue() {
+      var q = new PriorityQueue<>(List.of(1, 3, -1, 2, 4));
+      assertEquals(-1, q.peek());
+    }
+  }
+
+  @Nested
+  class DifferencesInMethods {
+    @Test
+    void useArrayDequeueMethods() {
+      var q = new ArrayDeque<>(List.of(1, 3, 2, 4));
+      assertEquals(1, q.getFirst());
+      assertEquals(4, q.getLast());
+    }
+
+    @Test
+    void useLinkedListMethods() {
+      var q = new LinkedList<>(List.of(1, 3, 2, 4));
+      assertEquals(1, q.getFirst());
+      assertEquals(4, q.getLast());
+    }
+
+    @Test
+    void usePriorityQueueMethods() {
+      var q = new PriorityQueue<>(List.of(1, 3, -1, 2, 4));
+      // No method to get first and last element.
+    }
+  }
 }
--- src/test/java/pl/edu/mimuw/SetDifferencesTest.java
+++ src/test/java/pl/edu/mimuw/SetDifferencesTest.java
@@ -1,5 +1,91 @@
 package pl.edu.mimuw;
 
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SetDifferencesTest {
 
+  @Nested
+  class DifferenceInIterationOrder {
+    @Test
+    void iterateTreeSet() {
+      Set<Integer> s = new TreeSet<>(List.of(5, 2, 3, 1, 4));
+      var z = new ArrayList<>(s);
+      assertEquals(List.of(1, 2, 3, 4, 5), z);
+    }
+
+    @Test
+    void iterateHashSet() {
+      Set<Integer> s = new HashSet<>(List.of(5, 2, 3, 1, 4));
+      var z = new ArrayList<>(s);
+
+      // The iterator goes through all the elements but in no specific order.
+      z.sort(Integer::compare);
+      assertEquals(List.of(1, 2, 3, 4, 5), z);
+    }
+
+    @Test
+    void iterateLinkedHashSet() {
+      Set<Integer> s = new LinkedHashSet<>(List.of(5, 2, 3, 1, 4));
+      var z = new ArrayList<>(s);
+      assertEquals(List.of(5, 2, 3, 1, 4), z);
+    }
+  }
+
+  @Nested
+  class DifferencesInMethods {
+    @Test
+    void testTreeSetMethods() {
+      var s = new TreeSet<>(List.of(5, 2, 3, 1, 4));
+      assertEquals(5, s.size());
+
+      // Only for the tree set.
+      assertEquals(1, s.first());
+      assertEquals(5, s.last());
+    }
+
+    @Test
+    void testHashSetMethods() {
+      var s = new HashSet<>(List.of(5, 2, 3, 1, 4));
+      assertEquals(5, s.size());
+    }
+
+    @Test
+    void testLinkedHashSetMethods() {
+      var s = new LinkedHashSet<>(List.of(5, 2, 3, 1, 4));
+      assertEquals(5, s.size());
+    }
+  }
+
+  @Nested
+  class DifferencesInAddingAnElementTwice {
+    @Test
+    void testAddingAnElementTwiceToATreeSet() {
+      var s = new TreeSet<>(List.of(5, 2, 3, 1, 4));
+      s.add(3);
+      assertEquals(5, s.size());
+      var z = new ArrayList<>(s);
+      assertEquals(List.of(1, 2, 3, 4, 5), z);
+    }
+
+    @Test
+    void testAddingAnElementTwiceToAHashSet() {
+      var s = new HashSet<>(List.of(5, 2, 3, 1, 4));
+      s.add(3);
+      assertEquals(5, s.size());
+    }
+
+    @Test
+    void testAddingAnElementTwiceToALinkedHashSet() {
+      var s = new LinkedHashSet<>(List.of(5, 2, 3, 1, 4));
+      s.add(3);
+      assertEquals(5, s.size());
+      var z = new ArrayList<>(s);
+      assertEquals(List.of(5, 2, 3, 1, 4), z);
+    }
+  }
 }
