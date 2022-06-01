764085cf713c5bd233aab74a57cb3c6eb88e495a
SzymonSzmgielski <szmigielski.szymon13@gmail.com>
2022-06-01 15:35:05 +0200

    class

--- src/test/java/pl/edu/mimuw/MapDifferencesTest.java
+++ src/test/java/pl/edu/mimuw/MapDifferencesTest.java
@@ -1,5 +1,74 @@
 package pl.edu.mimuw;
 
-class MapDifferencesTest {
+import java.util.Map;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class MapDifferencesTest {
+    @Nested
+    class DifferencesInKeySetOrder {
+        @Test
+        void keySetOfHashMap() {
+            Map<Integer, Integer> m = new HashMap<>();
+            m.put(2, 200);
+            m.put(1, 100);
+            m.put(3, 300);
+            Set<Integer> s = m.keySet();
+            Iterator<Integer> it = s.iterator();
+
+            assertEquals(1, it.next());
+            assertEquals(2, it.next());
+            assertEquals(3, it.next());
+        }
+
+        @Test
+        void keySetOfLinkedHashMap() {
+            Map<Integer, Integer> m = new LinkedHashMap<>();
+            m.put(2, 200);
+            m.put(1, 100);
+            m.put(3, 300);
+            Set<Integer> s = m.keySet();
+            Iterator<Integer> it = s.iterator();
+
+            assertEquals(2, it.next());
+            assertEquals(1, it.next());
+            assertEquals(3, it.next());
+        }
+        
+        @Test
+        void keySetOfTreeMap() {
+            Map<Integer, Integer> m = new TreeMap<>((l, r) -> r - l);
+            m.put(2, 200);
+            m.put(1, 100);
+            m.put(3, 300);
+            Set<Integer> s = m.keySet();
+            Iterator<Integer> it = s.iterator();
+
+            assertEquals(3, it.next());
+            assertEquals(2, it.next());
+            assertEquals(1, it.next());
+        }
+    }
+
+    @Nested
+    class DifferencesInMethods {
+        @Test
+        void methodOfTreeMap() {
+            TreeMap<Integer, Integer> m = new TreeMap<>();
+            m.put(2, 200);
+            m.put(1, 100);
+            m.put(4, 300);
+            
+            assertEquals(300, m.ceilingEntry(3).getValue());
+        }
+    }
 }
--- src/test/java/pl/edu/mimuw/SetDifferencesTest.java
+++ src/test/java/pl/edu/mimuw/SetDifferencesTest.java
@@ -1,5 +1,124 @@
 package pl.edu.mimuw;
 
-class SetDifferencesTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class SetDifferencesTest {
+    @Nested
+    class DifferenceInIterationOrder {
+
+        @Test
+        void iterateTreeSet() {
+            Set<Integer> s = new TreeSet<>((l, r) -> r - l);
+            s.add(2);
+            s.add(1);
+            s.add(3);
+            Iterator<Integer> it = s.iterator();
+
+            assertEquals(3, it.next());
+            assertEquals(2, it.next());
+            assertEquals(1, it.next());
+        }
+
+        @Test
+        void iterateHashSet() {
+            Set<Integer> s = new HashSet<>();
+            s.add(2);
+            s.add(1);
+            s.add(3);
+            Iterator<Integer> it = s.iterator();
+
+            assertEquals(1, it.next());
+            assertEquals(2, it.next());
+            assertEquals(3, it.next());
+        }
+
+        @Test
+        void iterateLinkedHashSet() {
+            Set<Integer> s = new LinkedHashSet<>();
+
+            s.add(2);
+            s.add(1);
+            s.add(3);
+            Iterator<Integer> it = s.iterator();
+
+            assertEquals(2, it.next());
+            assertEquals(1, it.next());
+            assertEquals(3, it.next());
+        }
+
+    }
+
+    @Nested
+    class DifferenceInAddRemove {
+        @Test
+        void addRemoveInTreeSet() {
+            Set<Integer> s = new TreeSet<>();
+            s.add(2);
+            s.add(1);
+            s.add(3);
+            Iterator<Integer> it = s.iterator();
+            int first = it.next();
+            s.remove(first);
+            s.add(first);
+            it = s.iterator();
+            int newfirst = it.next();
+            assertEquals(first, newfirst);
+        }
+
+        @Test
+        void addRemoveInHashSet() {
+            Set<Integer> s = new HashSet<>();
+            s.add(2);
+            s.add(1);
+            s.add(3);
+            Iterator<Integer> it = s.iterator();
+            int first = it.next();
+            s.remove(first);
+            s.add(first);
+            it = s.iterator();
+            int newfirst = it.next();
+            assertEquals(first, newfirst);
+        }
+
+        @Test
+        void addRemoveInLinkedHashSet() {
+            Set<Integer> s = new LinkedHashSet<>();
+
+            s.add(2);
+            s.add(1);
+            s.add(3);
+            Iterator<Integer> it = s.iterator();
+            int first = it.next();
+            s.remove(first);
+            s.add(first);
+            it = s.iterator();
+            int newfirst = it.next();
+            assertNotEquals(first, newfirst);
+        }
+    }
+
+    @Nested
+    class DifferenceInMethods {
+        @Test
+        void addRemoveInTreeSet() {
+            SortedSet<Integer> s = new TreeSet<>();
+            s.add(2);
+            s.add(1);
+            s.add(3);
+            SortedSet<Integer> newSet = s.subSet(1, 2);
+            assertEquals(1, newSet.size());
+        }
+    }
 }
